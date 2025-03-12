faba63377d9f3604bbb885a7245c4cf8061116f2
Anaïs <a.aerts@interface3.be>
2025-03-12 15:29:25 +0100

    ADD ASP

---
+++ UI/Mappers/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using UI.Models.Utilisateur;
+using BLL.Entities;
+
+namespace UI.Mappers
+{
+	internal static class Mapper
+	{
+		// Lister tous les Utilisateurs
+		public static UtilisateurListItem ToListItem(this Utilisateur utilisateur)
+		{
+			return new UtilisateurListItem
+			{
+				Email = utilisateur.Email,
+				Pseudo = utilisateur.Pseudo,
+				DateCreation = utilisateur.DateCreation
+			};
+		}
+	}
+}
---
+++ UI/Models/Utilisateur/UtilisateurListItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UI.Models.Utilisateur
+{
+	public class UtilisateurListItem
+	{
+		[ScaffoldColumn(false)]
+		public Guid UtilisateurId { get; set; }
+
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+
+		[Display(Name = "Pseudo")]
+		public string Pseudo { get; set; }
+
+		[Display(Name = "Date de création")]
+		public DateTime DateCreation { get; set; }
+	}
+}
